8661b1038d587b10bed6fe7ee31237ea16859029
Vxalware <jainasinth@gmail.com>
2026-01-11 19:27:24 +0800

    Create Loadstring.lua

---
+++ assets/Loadstring.lua
@@ -0,0 +1,2 @@
+-- First use of loadstringer after its deletion
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/CodeCache/refs/heads/main/assets/CCV3%20Test.lua",true))()
